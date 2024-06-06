7bce2b1afc68da01f5837a318d3d1db5bba731ee
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2024-06-06 08:10:22 -0400

    added listeners and can update scores(need to implement win and lose conds)

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,6 +4,11 @@ import "../styles/GameBoard.css";
 
 function GameBoard() {
   const [pokemonSprites, setPokemonSprites] = useState([]);
+  const [arrOfPressedCards, setArrOfPressedCards] = useState([0, 1, 2]);
+  const [lostRound, setLostRound] = useState(false);
+  const [currScore, setCurrScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [cardClicked, setCardClicked] = useState(false);
 
   useEffect(() => {
     async function fetchSprites() {
@@ -12,13 +17,33 @@ function GameBoard() {
     }
 
     fetchSprites();
-  }, []);
+  }, [cardClicked]);
 
   const imagesOnDisplay = pokemonSprites.map((sprite, index) => (
-    <li key={index}>
+    <li className="listElement" key={index} onClick={() => handleClick(index)}>
       <img src={`${sprite}`} alt={`Pokemon sprite ${index}`} />
     </li>
   ));
+
+  //add event listeners to all cards
+  function handleClick(index) {
+    //if new card, update score and add index to arrOfPressedCards else refresh scores
+    //randomize cards
+    console.log("list tag tht has been clicked->" + index);
+
+    if (arrOfPressedCards.includes(index)) {
+      // check if lost round
+      updateScores(true, setCurrScore, setBestScore, currScore, bestScore);
+      updateScoreTags(currScore, bestScore);
+      // setArrOfPressedCards([]);
+    }
+    // else {
+    //   // otherwise, won round
+    //   setArrOfPressedCards((prevArr) => [...prevArr, index]); // update array
+    //   updateScores(true, setCurrScore, setBestScore, currScore, bestScore);
+    // }
+    // setCardClicked(true);
+  }
 
   return (
     <div className="gameboard">
@@ -27,4 +52,21 @@ function GameBoard() {
   );
 }
 
+function updateScores(won, setCurrScore, setBestScore, currScore, bestScore) {
+  if (won) {
+    setCurrScore((prevScore) => prevScore + 1);
+    let newCurrScore = currScore + 1;
+    if (newCurrScore >= bestScore) {
+      setBestScore(newCurrScore);
+    }
+  } else if (!won) {
+    setCurrScore(0);
+  }
+}
+function updateScoreTags(currScore, bestScore) {
+  let scoreTag = document.querySelector(".current-score");
+  let bestScoreTag = document.querySelector(".best-score");
+  scoreTag.textContent = `Score: ${currScore}`;
+  bestScoreTag.textContent = `Best Score: ${bestScore}`;
+}
 export default GameBoard;
