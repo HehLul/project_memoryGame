484c74f79a94a9fe1f7ff46ba0aafa1bbd094695
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2024-06-06 10:40:56 -0400

    Fixed duplicate sprite and key bug

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,43 +1,34 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import fetchApi from "../fetchApi";
 import "../styles/GameBoard.css";
 
 function GameBoard({ currScore, bestScore, setCurrScore, setBestScore }) {
   const [pokemonSprites, setPokemonSprites] = useState([]);
   const [arrOfPressedCards, setArrOfPressedCards] = useState([]);
-  const [lostRound, setLostRound] = useState(false);
   const [cardClicked, setCardClicked] = useState(false);
 
   useEffect(() => {
     async function fetchSprites() {
       const sprites = await fetchApi();
       setPokemonSprites(sprites);
-      // console.log(sprites);
     }
 
     fetchSprites();
     setCardClicked(false);
   }, [cardClicked]);
 
-  const imagesOnDisplay = pokemonSprites.map((sprite) => (
+  const imagesOnDisplay = pokemonSprites.map((sprite, index) => (
     <li
       className="listElement"
-      key={sprite.id}
+      key={`${sprite.id}-${index}`}
       onClick={() => handleClick(sprite.id)}
     >
-      <h3>{sprite.id}</h3>
-      <img src={`${sprite.sprite}`} alt={`Pokemon sprite ${sprite.id}`} />
+      <img src={sprite.sprite} alt={`Pokemon sprite ${sprite.id}`} />
     </li>
   ));
 
-  //add event listeners to all cards
-
   function handleClick(index) {
-    //if new card, update score and add index to arrOfPressedCards else refresh scores
-    //randomize cards
-    console.log("list tag that has been clicked->" + index);
     setArrOfPressedCards((prevArr) => [...prevArr, index]);
-    console.log("arrayOfPressedCards before if cond->" + arrOfPressedCards);
     if (arrOfPressedCards.includes(index)) {
       alert(`Lost at Score: ${currScore}`);
       let [newCurrScore, newBestScore] = updateScores(
@@ -50,7 +41,6 @@ function GameBoard({ currScore, bestScore, setCurrScore, setBestScore }) {
       updateScoreTags(newCurrScore, newBestScore);
       setArrOfPressedCards([]);
     } else {
-      console.log("WON ROUND!");
       let [newCurrScore, newBestScore] = updateScores(
         true,
         setCurrScore,
@@ -59,9 +49,6 @@ function GameBoard({ currScore, bestScore, setCurrScore, setBestScore }) {
         bestScore
       );
       updateScoreTags(newCurrScore, newBestScore);
-      // otherwise, won round
-      // setArrOfPressedCards((prevArr) => [...prevArr, index]); // update array
-      // updateScores(true, setCurrScore, setBestScore, currScore, bestScore);
     }
     setCardClicked(true);
   }
@@ -79,14 +66,13 @@ function updateScores(won, setCurrScore, setBestScore, currScore, bestScore) {
   if (won) {
     setCurrScore((prevScore) => prevScore + 1);
     newCurrScore = currScore + 1;
-    console.log("newScore->" + newCurrScore);
     if (newCurrScore >= bestScore) {
       newBestScore = newCurrScore;
       setBestScore(newCurrScore);
     } else {
       newBestScore = bestScore;
     }
-  } else if (!won) {
+  } else {
     setCurrScore(0);
     newCurrScore = 0;
     newBestScore = bestScore;
@@ -94,10 +80,12 @@ function updateScores(won, setCurrScore, setBestScore, currScore, bestScore) {
 
   return [newCurrScore, newBestScore];
 }
+
 function updateScoreTags(currScore, bestScore) {
   let scoreTag = document.querySelector(".current-score");
   let bestScoreTag = document.querySelector(".best-score");
   scoreTag.textContent = `Score: ${currScore}`;
   bestScoreTag.textContent = `Best Score: ${bestScore}`;
 }
+
 export default GameBoard;
--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,44 +1,39 @@
+const listOfPokemonNames = [
+  "charizard",
+  "charmander",
+  "pikachu",
+  "gengar",
+  "eevee",
+  "lugia",
+  "garchomp",
+  "dragonite",
+  "blastoise",
+  "snorlax",
+];
+
 async function fetchApi() {
   let arrayOfPokemonImages = [];
-  const listOfPokemonNames = [
-    "charizard",
-    "charmander",
-    "pikachu",
-    "gengar",
-    "eevee",
-    "lugia",
-    "garchomp",
-    "dragonite",
-    "blastoise",
-    "snorlax",
-  ];
+
   shuffle(listOfPokemonNames);
   for (let i = 0; i < listOfPokemonNames.length; i++) {
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${listOfPokemonNames[i]}`
     );
     const responseJson = await response.json();
-    // console.log(responseJson);
     const sprite = responseJson.sprites.front_shiny;
     const id = responseJson.id;
-    // console.log(sprite);
-    // console.log(id);
     arrayOfPokemonImages.push({ id, sprite });
   }
-  // console.log(arrayOfPokemonImages);
   return arrayOfPokemonImages;
 }
 
 function shuffle(array) {
   let currentIndex = array.length;
 
-  // While there remain elements to shuffle...
   while (currentIndex != 0) {
-    // Pick a remaining element...
     let randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
-    // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
       array[currentIndex],
@@ -46,4 +41,5 @@ function shuffle(array) {
   }
   return array;
 }
+
 export default fetchApi;
